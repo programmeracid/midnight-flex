e55bb23e2320666a34ff2082c43e468dc40e6013
programmeracid <siddhubuddha2005@gmail.com>
2025-02-24 21:18:11 +0530

    checking base url

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -2,6 +2,7 @@ import { create } from "zustand";
 
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+console.log("BASE_URL : " + BASE_URL)
 
 export const useAuthStore = create((set) => ({
     user: null,
--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(_dirname, "../client/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(_dirname, "../client", "dist", "index.html"));
     })
 }
 
